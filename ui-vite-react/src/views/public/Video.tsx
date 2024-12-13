6c949d675c0d465baaed3421e22fdedb5a83b41f
gogpa <gogpa@email.com>
2024-12-13 17:47:01 +0800

    a

--- ui-vite-react/src/views/public/Video.tsx
+++ ui-vite-react/src/views/public/Video.tsx
@@ -1,16 +1,25 @@
-import { Layout } from "antd";
-import { Link } from "react-router-dom";
+import React, { useState, useCallback } from "react";
+import { Layout, Row, Col, List, Typography } from "antd";
 import "./Video.scss";
-const { Header, Footer, Sider, Content } = Layout;
+const { Sider, Content } = Layout;
 const Video = () => {
+    const data = {
+        title: "Elon Musk's little son.",
+        subtitle: [
+            { start: 0, end: 2.5, text: "That kid. Oh." },
+            { start: 2.8, end: 5.5, text: "Hey what are you doing?" },
+            { start: 8.0, end: 10.5, text: "Do you want to just sit on my knee or something?" },
+            { start: 11.0, end: 15.0, text: "OK I guess so. All right. We got mini me here." },
+        ],
+    };
+    const [videoTitle, setVideoTitle] = useState(data["title"]);
+    const [videoSubtitle, setVideoSubtitle] = useState(data["subtitle"]);
     return (
         <Layout>
-            <Sider>
-                1
+            <Content className="main">2</Content>
+            <Sider className="sider">
+                <List header={<div>{videoTitle}</div>} footer={<div>Footer</div>} bordered dataSource={videoSubtitle} renderItem={(item) => <List.Item>{item.text}</List.Item>} />
             </Sider>
-            <Content>2
-                
-            </Content>
         </Layout>
     );
 };
