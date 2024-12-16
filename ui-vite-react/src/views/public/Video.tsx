d9bf45b8b0b29c8da539385b204e5bf105017230
gogpa <gogpa@email.com>
2024-12-16 17:42:53 +0800

    a

--- ui-vite-react/src/views/public/Video.tsx
+++ ui-vite-react/src/views/public/Video.tsx
@@ -9,42 +9,11 @@ const Video = () => {
     const [title, setTitle] = useState("Elon Musk's little son.");
     const [subtitle, setSubtitle] = useState([
         { startTime: "00:00:00", endTime: "00:00:02,500", text: "That kid. Oh." },
-        { startTime: "00:02:80", endTime: "00:05:50", text: "Hey what are you doing?" },
-        { startTime: "00:08:00", endTime: "00:10:50", text: "Do you want to just sit on my knee or something?" },
-        { startTime: "00:11:00", endTime: "00:15:00", text: "OK I guess so. All right. We got mini me here." },
-        { startTime: "00:00:00", endTime: "00:02:50", text: "That kid. Oh." },
-        { startTime: "00:02:80", endTime: "00:05:50", text: "Hey what are you doing?" },
-        { startTime: "00:08:00", endTime: "00:10:50", text: "Do you want to just sit on my knee or something?" },
-        { startTime: "00:11:00", endTime: "00:15:00", text: "OK I guess so. All right. We got mini me here." },
-        { startTime: "00:00:00", endTime: "00:02:50", text: "That kid. Oh." },
-        { startTime: "00:02:80", endTime: "00:05:50", text: "Hey what are you doing?" },
-        { startTime: "00:08:00", endTime: "00:10:50", text: "Do you want to just sit on my knee or something?" },
-        { startTime: "00:11:00", endTime: "00:15:00", text: "OK I guess so. All right. We got mini me here." },
-        { startTime: "00:00:00", endTime: "00:02:50", text: "That kid. Oh." },
-        { startTime: "00:02:80", endTime: "00:05:50", text: "Hey what are you doing?" },
-        { startTime: "00:08:00", endTime: "00:10:50", text: "Do you want to just sit on my knee or something?" },
-        { startTime: "00:11:00", endTime: "00:15:00", text: "OK I guess so. All right. We got mini me here." },
-        { startTime: "00:00:00", endTime: "00:02:50", text: "That kid. Oh." },
-        { startTime: "00:02:80", endTime: "00:05:50", text: "Hey what are you doing?" },
-        { startTime: "00:08:00", endTime: "00:10:50", text: "Do you want to just sit on my knee or something?" },
-        { startTime: "00:11:00", endTime: "00:15:00", text: "OK I guess so. All right. We got mini me here." },
-        { startTime: "00:00:00", endTime: "00:02:50", text: "That kid. Oh." },
-        { startTime: "00:02:80", endTime: "00:05:50", text: "Hey what are you doing?" },
-        { startTime: "00:08:00", endTime: "00:10:50", text: "Do you want to just sit on my knee or something?" },
-        { startTime: "00:11:00", endTime: "00:15:00", text: "OK I guess so. All right. We got mini me here." },
-        { startTime: "00:00:00", endTime: "00:02:50", text: "That kid. Oh." },
-        { startTime: "00:02:80", endTime: "00:05:50", text: "Hey what are you doing?" },
-        { startTime: "00:08:00", endTime: "00:10:50", text: "Do you want to just sit on my knee or something?" },
-        { startTime: "00:11:00", endTime: "00:15:00", text: "OK I guess so. All right. We got mini me here." },
-        { startTime: "00:00:00", endTime: "00:02:50", text: "That kid. Oh." },
-        { startTime: "00:02:80", endTime: "00:05:50", text: "Hey what are you doing?" },
-        { startTime: "00:08:00", endTime: "00:10:50", text: "Do you want to just sit on my knee or something?" },
-        { startTime: "00:11:00", endTime: "00:15:00", text: "OK I guess so. All right. We got mini me here." },
-        { startTime: "00:00:00", endTime: "00:02:50", text: "That kid. Oh." },
-        { startTime: "00:02:80", endTime: "00:05:50", text: "Hey what are you doing?" },
-        { startTime: "00:08:00", endTime: "00:10:50", text: "Do you want to just sit on my knee or something?" },
-        { startTime: "00:11:00", endTime: "00:15:00", text: "OK I guess so. All right. We got mini me here." },
+        { startTime: "00:02:80", endTime: "00:00:05,500", text: "Hey what are you doing?" },
+        { startTime: "00:08:00", endTime: "00:00:10,500", text: "Do you want to just sit on my knee or something?" },
+        { startTime: "00:11:00", endTime: "00:00:15,000", text: "OK I guess so. All right. We got mini me here." },
     ]);
+    const [subtitleInput, setSubtitleInput] = useState("");
     const refVideo = useRef<HTMLVideoElement>(null);
     const handleChangeStartTime = (event: any, index: number) => {
         const newSubtitle = subtitle.map((v, k, a) => {
@@ -94,16 +63,26 @@ const Video = () => {
     const handleTimeUpdate = (e: any) => {
         console.log("currentSubtitle: ", currentSubtitle);
         console.log("currentTime:", e.target.currentTime);
-        const endTimeArr = subtitle[currentSubtitle].endTime.split(":");
-        const endTime = parseFloat(endTimeArr[2].replace(/,/g, "."));
-        if (currentSubtitle < subtitle.length && e.target.currentTime >= endTime) {
-            refVideo.current?.pause();
-            console.log("refVideo paused");
+        if (currentSubtitle < subtitle.length) {
+            const endTimeArr = subtitle[currentSubtitle].endTime.split(":");
+            const endTime = parseFloat(endTimeArr[2].replace(/,/g, "."));
+            if (e.target.currentTime >= endTime) {
+                refVideo.current?.pause();
+                console.log("refVideo paused");
+            }
         }
     };
     const handleEnded = () => {};
     const handlePause = () => {};
-
+    const handleInputSubtitle = (e: any) => {
+        if (subtitle[currentSubtitle].text === e.target.value) {
+            setSubtitleInput("");
+            setCurrentSubtitle(currentSubtitle + 1);
+            refVideo.current?.play();
+        } else {
+            setSubtitleInput(e.target.value);
+        }
+    };
     return (
         <Layout style={{ minWidth: "1200px", height: "100%" }}>
             <Sider width="600" style={{ flex: "0 0 50%", maxWidth: "none", minWidth: "600px", width: "50%", backgroundColor: "#fff" }}>
@@ -124,10 +103,12 @@ const Video = () => {
                 </Scrollbars>
             </Sider>
             <Content style={{ background: "#ccc" }}>
-                <Button onClick={handleTest}>Default Button</Button>
-                <video id="video" controls onPause={handlePause} onEnded={handleEnded} onTimeUpdate={handleTimeUpdate} ref={refVideo}>
-                    <source src="/src/assets/Elon.mp4" type="video/mp4" /> Your browser does not support video tag.
-                </video>
+                <Space size="small" direction="vertical" style={{ width: "100%", height: "100%", alignItems: "stretch", justifyContent: "space-between" }}>
+                    <video style={{ maxWidth: "600px", maxHeight: "400px", textAlign: "center" }} id="video" controls onPause={handlePause} onEnded={handleEnded} onTimeUpdate={handleTimeUpdate} ref={refVideo}>
+                        <source src="/src/assets/Elon.mp4" type="video/mp4" /> Your browser does not support video tag.
+                    </video>
+                    <TextArea style={{ borderRadius: "0" }} value={subtitleInput} onChange={(e) => handleInputSubtitle(e)} />
+                </Space>
             </Content>
         </Layout>
     );
